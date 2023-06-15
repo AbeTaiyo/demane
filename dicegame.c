5a0663656a94f626c5072b0cc18d21971308049b
s1300108 <s1300108@ilab1dc46.u-aizu.ac.jp>
2023-06-15 16:00:46 +0900

    Dice game: initial revision

---
+++ dicegame.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+
+    int a,b,num;
+
+    a = rand() %6 + 1;
+    b = rand() %6 + 1;
+
+    num = a+b;
+
+    printf("Rolling dice...\n");
+    printf("Die 1: %d\n",a);
+    printf("Die 2: %d\n",b);
+    printf("Total value: %d\n",num);
+
+    return 0;
+}
